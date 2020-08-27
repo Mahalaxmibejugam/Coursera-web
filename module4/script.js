cad51a9fd6635458e2b048002dab0e79287e30c8
Mahalaxmi <mahalaxmibejugam@gmail.com>
2020-08-27 19:22:41 +0530

    Fourth module

---
+++ module4/script.js
@@ -0,0 +1,10 @@
+(function(){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var name in names) {
+      if (names[name].charAt(0)=='j' || names[name].charAt(0)=='J') {
+          byeSpeaker.speak(names[name])
+      } else {
+          helloSpeaker.speak(names[name])
+  }
+}
+})();
